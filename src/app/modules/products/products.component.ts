e4045c1411d9f9dcebdab9d2c440d76e62e36290
Mateo Rios Diaz <mateorios993@gmail.com>
2024-02-22 08:18:34 -0500

    Se hace uso del component product en la lista de products

--- src/app/modules/products/products.component.ts
+++ src/app/modules/products/products.component.ts
@@ -4,11 +4,12 @@ import { HttpClientModule } from '@angular/common/http';
 import { Product } from '../../model/product.model';
 import { CommonModule } from '@angular/common';
 import { LoadingComponent } from '../../components/icons/loading/loading.component';
+import { ProductComponent } from './components/product/product.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule, HttpClientModule, LoadingComponent],
+  imports: [CommonModule, HttpClientModule, LoadingComponent, ProductComponent],
   providers: [PlatziService],
   templateUrl: './products.component.html',
   styleUrl: './products.component.css'
